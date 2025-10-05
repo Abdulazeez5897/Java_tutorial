683d4df266bf3de8292045468ca8ebc990d6abe5
Abdulazeez5897 <137459978+Abdulazeez5897@users.noreply.github.com>
2025-10-05 16:50:32 +0100

    Update chapterOne_Excercise1.java

--- chapterOne_Excercise1.java
+++ chapterOne_Excercise1.java
@@ -1,3 +1,4 @@
+// part one
 class shuffle1 {
     public static void main(String[] args) {
         int x = 3;
@@ -18,4 +19,78 @@ class shuffle1 {
             }
         }
     }
+}
+// part two
+
+// A
+class Exercise1b {
+ public static void main(String [] args) {
+ int x = 1;
+ while ( x < 10 ) {
+ if ( x > 3) {
+ System.out.println(“big x”);
+ }
+ }
+ }
+}
+// This will compile without an output... and loop continues till infinity
+// How to fix
+class Exercise1b {
+ public static void main(String [] args) {
+ int x = 1;
+ while ( x < 10 ) {
+     x = x + 1;
+ if ( x > 3) {
+ System.out.println(“big x”);
+ }
+ }
+ }
+}
+
+//B
+public static void main(String [] args) {
+ int x = 5;
+ while ( x > 1 ) {
+ x = x - 1; //Add this line
+ if ( x < 3) {
+ System.out.println(“small x”);
+ }
+ }
+}
+// This code won't run without a class declaration
+// How to fix
+class Excercise2b { //Add this line
+public static void main(String [] args) {
+ int x = 5;
+ while ( x > 1 ) {
+ x = x - 1;
+ if ( x < 3) {
+ System.out.println(“small x”);
+ }
+ }
+}
+}
+
+//C
+class Exercise1b {
+ int x = 5;
+ while ( x > 1 ) {
+ x = x - 1;
+ if ( x < 3) {
+ System.out.println(“small x”);
+ }
+ }
+}
+//This code won't run without the main method
+//How to fix
+class Exercise1b {
+    public static void main(string [] args) {
+        int x = 5;
+ while ( x > 1 ) {
+ x = x - 1;
+ if ( x < 3) {
+ System.out.println(“small x”);
+ }
+ }
+}
 }
